f158cc73f7a98a98d307364df566eba69e8f1e5c
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-07-31 19:27:36 +0900

    [Update] 챌린지 추가
    input에 달러를 넣고 선택한 코인의 개수를 몇개를 살 수 있는지 확인할 수 있도록 update

--- coin_tracker/src/App.js
+++ coin_tracker/src/App.js
@@ -3,6 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [buycoin, setBuycoin] = useState(0);
+  const onChangeMoney = (event) => {
+    setMoney(event.target.value);
+  };
+  const onSubmitMoney = (event) => {
+    event.preventDefault();
+  };
+  const onChangeCoin = (event) => {
+    setBuycoin(event.target.value.split(" ")[3]);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -14,11 +25,19 @@ function App() {
 
   return (
     <div>
+      <input
+        value={money}
+        type="text"
+        placeholder="Write you have money"
+        onChange={onChangeMoney}
+      />
+
+      <h1>{money / buycoin}</h1>
       <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onChangeCoin}>
           {coins.map((coin, index) => (
             <option key={index}>
               {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
